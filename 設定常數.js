928641e85e4a0e5d301f899eb13c337002e19508
zhaoen <zhaoen@capsulecorporation.cc>
2026-04-01 11:07:58 +0800

    fix: 重新命名欄位

--- 設定常數.js
+++ 設定常數.js
@@ -6,5 +6,5 @@
  * Change these to match the column names you are using for email
  * recipient addresses and email sent column.
  */
-const RECIPIENT_COL = "收件人";
+const RECIPIENT_COL = "收件人信箱";
 const EMAIL_SENT_COL = "已寄出";
